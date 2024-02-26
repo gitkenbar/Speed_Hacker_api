33b9940065b2f29a331b99cd781dd28d0e9cd2c4
gitkenbar <kennethbarker12@gmail.com>
2024-02-26 15:04:10 -0600

    minor formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   #validations
-  validates :username, presence:true, uniqueness:true, length: {minimum: 3, maximum: 20}
+    validates :username, presence:true, uniqueness:true, length: {minimum: 3, maximum: 20}
     validates :password, presence:true, length: {minimum: 6, maximum: 25}
   
-  has_many :languages
-  has_many :games
-  has_many :scores
+  #associations
+    has_many :languages
+    has_many :games
+    has_many :scores
   #Issue: cannot create user with multiple languages
 
-  #possible solution: Create a UserLanguage join table that can hold user_ids and language_ids
+  #possible solution: Create a LanguageUser join table that can hold user_ids and language_ids
 end
