d957ec113b614bcc23c5fcccebb2878ecfd0df75
gitkenbar <kennethbarker12@gmail.com>
2024-06-20 17:05:25 -0500

    organized seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,27 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 bigBang = Time.now;
 
-admin = User.create(
-    username: "test",
-    password: "password"
+# Create Users
+
+    admin = User.create(
+        username: "admin",
+        password: "password"
     )
-    
+  
+    5.times do |i|
+        user = User.create(
+            username: Faker::Internet.username(specifier: 3..10, separators: %w(_)),
+            password: 'password'
+            );     
+    end
+
+    user = User.find_by(id: 2)
+
+# Create Contents
+
 ezpz = Content.create(
+    # 'Bad' example, this format is used for multi-word inputs
+    # this should be a %w array
     challenge: ["a","b","c","d","e"]
 )
 
@@ -20,7 +35,27 @@ foxy = Content.create(
     challenge: %w[The very swift fox jumps over the lazy dog]
 )
 
-testgame = admin.games.create(
+fruitAlphabet = Content.create(
+    challenge: %w[
+        Apple Banana Cherry DragonFruit Elderberry Fig Gooseberry Honeydew Ita Palm Java Apple Kiwi Lemon Mango Nectarine Orange Pomegranate Quince Raspberry Strawberry Tomato Uglifruit Vanilla Watermelon Xigua Yuzu Zucchini
+    ]
+)
+
+vege_alphabet = Content.create(
+    challenge: %w[
+        Aubergine Broccoli Carrot Daikon Radish Edamame Fennel Garlic Horseradish Iceburg Lettuce Jalapeno Kale Leek Mushroom Napa Cabbage Onion Pumpkin Queen Radish Squash Turnip Underground Viking Watercress XRay Yam Zucchini
+    ]
+)
+
+5.times do |i|
+    Content.create(
+        challenge: [Faker::Lorem.sentence, Faker::Lorem.sentence, Faker::Lorem.sentence, Faker::Lorem.sentence, Faker::Lorem.sentence]
+        );
+    end
+
+# Create Games
+
+admin.games.create(
     name: "ezpz",
     content_id: 1
     )
@@ -29,47 +64,21 @@ admin.games.create(
     name: "Full Alphabet",
     content_id: 2
     )
+    
 admin.games.create(
     name: "Foxy",
     content_id: 3
 )
-
-5.times do |i|
-    user = User.create(
-        username: Faker::Internet.username(specifier: 3..10, separators: %w(_)),
-        password: 'password'
-        );     
-end
-
-user = User.find_by(id: 2)
-
-dune = Content.create(
-    challenge: [
-        Faker::Books::Dune.quote, Faker::Books::Dune.quote, Faker::Books::Dune.quote, Faker::Books::Dune.quote, Faker::Books::Dune.quote
-    ])  
-
-fruitAlphabet = Content.create(
-    challenge: [
-        "Apple", "Banana", "Cherry", "DragonFruit", "Elderberry", "Fig", "Gooseberry", "Honeydew", "Ita Palm", "Java Apple", "Kiwi", "Lemon", "Mango", "Nectarine", "Orange", "Pomegranate", "Quince", "Raspberry", "Strawberry", "Tomato", "Uglifruit", "Vanilla", "Watermelon", "Xigua", "Yuzu", "Zucchini"
-    ]
-)
-
-duneGame = user.games.create(
-    name: Faker::Lorem.sentence,
-    content_id: 2
-    );
 
 fruitAlphabetGame = user.games.create(
     name: "FruitAlphabet",
     content_id: 4
 )
 
-             
-5.times do |i|
-    Content.create(
-        challenge: [Faker::Lorem.sentence, Faker::Lorem.sentence, Faker::Lorem.sentence, Faker::Lorem.sentence, Faker::Lorem.sentence]
-        );
-    end
+vegeAlphabet_game = user.games.create(
+    name: "Veg Alphabet",
+    content_id: 5
+)
 
 userGames = user.games.create(
     name: Faker::Lorem.sentence,
@@ -77,6 +86,9 @@ userGames = user.games.create(
     user_id: 3
     );
 
+    
+ # Create Scores
+    
 5.times do |i|
     Score.create(
         score: i,
@@ -90,11 +102,13 @@ Score.create(
     user_id: 1,
     game_id: 1
 )
+
 Score.create(
     score: 2,
     user_id: 1,
     game_id: 1
 )
+
 Score.create(
     score: 3,
     user_id: 1,
